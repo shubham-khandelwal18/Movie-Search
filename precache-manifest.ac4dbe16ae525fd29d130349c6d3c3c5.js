b8cc433f0932786a1be00c9aa01abafa03553753
Shubham Khandelwal <shubhamsharma18082001@gmail.com>
2020-07-11 02:15:35 +0530

    Updates

---
+++ precache-manifest.ac4dbe16ae525fd29d130349c6d3c3c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bf34f08daab02df052102e984597d7",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "b54aed87ff2d692cdf23",
+    "url": "/Movie-Search/static/css/main.fe7f0303.chunk.css"
+  },
+  {
+    "revision": "6da2a510edb02fca0f90",
+    "url": "/Movie-Search/static/js/2.59f47aeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search/static/js/2.59f47aeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b54aed87ff2d692cdf23",
+    "url": "/Movie-Search/static/js/main.c1356fd0.chunk.js"
+  },
+  {
+    "revision": "6ba6c53493c6e13ae9bf",
+    "url": "/Movie-Search/static/js/runtime-main.0edac934.js"
+  }
+]);
