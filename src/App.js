4e88de69d7bea55b7754c3f018b4973ce0e0276d
Shubham Khandelwal <shubhamsharma18082001@gmail.com>
2020-07-11 02:18:37 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React,{useState} from 'react';
+import Search from './components/Search.js';
+import axios from 'axios';
+import Results from './components/Results';
+import Popup from './components/Popup.js';
+function App(){
+  const [state,setState]=useState({
+    s:"",
+    p:"",
+    results:[],
+    selected:{}
+  });  
+  const apiurl="http://www.omdbapi.com/?apikey=83b255de";
+  const search=(e)=>{
+    if(e.key==="Enter"){
+      axios(apiurl+"&s="+state.s).then(({data})=>{
+        let results =data.Search;
+        setState(prevState=>{
+          return {...prevState,results:results}
+        })
+        setState(prevState=>{
+          let s=state.s
+          return {...prevState,p:s}
+        })
+      });
+    }
+  }
+  const handleInput=(e)=>{
+    let s=e.target.value;
+    setState(prevState=>{
+      return {...prevState,s: s}
+    })
+  }
+  const openPopup=id=>{
+    axios(apiurl+"&i="+id).then(({data})=>{
+      let result=data;
+      console.log(result);
+      setState(prevState=>{
+        return {...prevState,selected:result}
+      });
+    });
+  }
+  const closePopup=()=>{
+    setState(prevState=>{
+      return {...prevState,selected:{}}
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Movie Portal</h1>
       </header>
+      <main>
+         <Search handleInput={handleInput} search={search}/>
+         {state.results?<Results results={state.results} openPopup={openPopup}/>:
+         <div>
+         <h1 className="fex">No results found for {state.p}</h1>
+         <h2 className="dex">Note: Press Enter everytime you want to search </h2>
+         </div>
+       }
+         {(typeof state.selected.Title!="undefined") ? <Popup selected={state.selected} closePopup=
+          {closePopup}/> : false}
+      </main>
     </div>
   );
 }
